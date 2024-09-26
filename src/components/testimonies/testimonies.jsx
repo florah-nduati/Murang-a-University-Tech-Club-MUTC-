d9c97a8109e3e5eb7a0e0e5b9e34b76e92c21d73
git <florah@gmail.com>
2024-09-26 19:52:10 +0300

    add carousel

--- src/components/testimonies/testimonies.jsx
+++ src/components/testimonies/testimonies.jsx
@@ -1,13 +1,14 @@
 import "./testimonies.css";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css'; 
-import customer1 from "../../assets/alumni1.jpg";
-function TestimoniesCard({ customerAvater, customerName, title, text }) {
+import "react-multi-carousel/lib/styles.css";
+import Carousel from "react-multi-carousel";
+import testimonies from "../../data/testimonies";
+
+function TestimonyCard({ alumniAvater, alumniName, title, text }) {
   return (
     <div className="testimony-card">
       <div className="testimony-card-details">
-        <img src={customerAvater} alt={customerName} />
-        <h4 className="testimony-card-customer-name">{customerName}</h4>
+        <img src={alumniAvater} alt={alumniName} />
+        <h4 className="testimony-card-customer-name">{alumniName}</h4>
       </div>
       <h4 className="testimony-title">{title}</h4>
       <p className="testimony-text">{text}</p>
@@ -16,17 +17,22 @@ function TestimoniesCard({ customerAvater, customerName, title, text }) {
 }
 function Testimonies() {
   return (
-    <section className="testimonies-section">
-      <h2 className="title">testimonies</h2>
-      <div className="testimonies-container">
-        <TestimoniesCard
-          customerAvater={customer1}
-          customerName="lucy wambui"
-          title="Web Developer and Alumni"
-          text="MUTC gave me the foundation I needed to become a professional web developer. The hands-on workshops and collaborative projects not only boosted my technical skills but also connected me with a supportive network of like-minded individuals. I owe my career success to the experience and mentorship I gained from the club."
-        />
-      </div>
-    </section>
+    <Carousel>
+      <section className="testimonies-section">
+        <h2 className="title">testimonies</h2>
+        <div className="testimonies-container">
+          {Testimonies.map((testimony, i) => (
+            <TestimonyCard
+              key={i}
+              alumniAvater={testimony.alumniAvater}
+              alumniName={testimony.alumniName}
+              title={testimony.title}
+              text={testimony.text}
+            />
+          ))}
+        </div>
+      </section>
+    </Carousel>
   );
 }
 
---
+++ src/data/testimonies.js
@@ -0,0 +1,61 @@
+import alumni1 from "../../assets/alumni1.jpg";
+import alumni2 from "../../assets/alumni2.jpg";
+import alumni3 from "../../assets/alumni3.jpg";
+import alumni4 from "../../assets/alumni4.jpg";
+import alumni5 from "../../assets/alumni5.jpg";
+import alumni6 from "../../assets/alumni6.jpg";
+import alumni7 from "../../assets/alumni7.jpg";
+import alumni8 from "../../assets/alumni8.jpg";
+
+const testimonies = [
+  {
+    alumniAvater: alumni1,
+    alumniName: "lucy wambui",
+    title: "Web Developer and Alumni",
+    text: "MUTC gave me the foundation I needed to become a professional web developer. The hands-on workshops and collaborative projects not only boosted my technical skills but also connected me with a supportive network of like-minded individuals. I owe my career success to the experience and mentorship I gained from the club.",
+  },
+  {
+    alumniAvater: alumni2,
+    alumniName: "grace gitau",
+    title: "Web Developer and Alumni",
+    text: "MUTC gave me the foundation I needed to become a professional web developer. The hands-on workshops and collaborative projects not only boosted my technical skills but also connected me with a supportive network of like-minded individuals. I owe my career success to the experience and mentorship I gained from the club.",
+  },
+  {
+    alumniAvater: alumni3,
+    alumniName: "florah nduati",
+    title: "Web Developer and Alumni",
+    text: "MUTC gave me the foundation I needed to become a professional web developer. The hands-on workshops and collaborative projects not only boosted my technical skills but also connected me with a supportive network of like-minded individuals. I owe my career success to the experience and mentorship I gained from the club.",
+  },
+  {
+    alumniAvater: alumni4,
+    alumniName: "mirriana gidayi",
+    title: "Web Developer and Alumni",
+    text: "MUTC gave me the foundation I needed to become a professional web developer. The hands-on workshops and collaborative projects not only boosted my technical skills but also connected me with a supportive network of like-minded individuals. I owe my career success to the experience and mentorship I gained from the club.",
+  },
+  {
+    alumniAvater: alumni5,
+    alumniName: "mary wanjiru",
+    title: "Web Developer and Alumni",
+    text: "MUTC gave me the foundation I needed to become a professional web developer. The hands-on workshops and collaborative projects not only boosted my technical skills but also connected me with a supportive network of like-minded individuals. I owe my career success to the experience and mentorship I gained from the club.",
+  },
+  {
+    alumniAvater: alumni6,
+    alumniName: "florida gatwiri",
+    title: "Web Developer and Alumni",
+    text: "MUTC gave me the foundation I needed to become a professional web developer. The hands-on workshops and collaborative projects not only boosted my technical skills but also connected me with a supportive network of like-minded individuals. I owe my career success to the experience and mentorship I gained from the club.",
+  },
+  {
+    alumniAvater: alumni7,
+    alumniName: "moses mukabi",
+    title: "Web Developer and Alumni",
+    text: "MUTC gave me the foundation I needed to become a professional web developer. The hands-on workshops and collaborative projects not only boosted my technical skills but also connected me with a supportive network of like-minded individuals. I owe my career success to the experience and mentorship I gained from the club.",
+  },
+  {
+    alumniAvater: alumni8,
+    alumniName: "moses nzioka",
+    title: "Web Developer and Alumni",
+    text: "MUTC gave me the foundation I needed to become a professional web developer. The hands-on workshops and collaborative projects not only boosted my technical skills but also connected me with a supportive network of like-minded individuals. I owe my career success to the experience and mentorship I gained from the club.",
+  },
+];
+
+export default testimonies;
