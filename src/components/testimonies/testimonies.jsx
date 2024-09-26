ebe741b1effa54badbdac703cf3cbfbc5e2a7096
git <florah@gmail.com>
2024-09-26 22:08:43 +0300

    arrange code

--- src/components/testimonies/testimonies.jsx
+++ src/components/testimonies/testimonies.jsx
@@ -5,22 +5,22 @@ import "react-multi-carousel/lib/styles.css";
 //import alumni1 from "../../assets/alumni1.jpg";
 
 const responsive = {
-    desktop: {
-      breakpoint: { max: 4000, min: 1024 },
-      items: 3,
-      slidesToSlide: 2 // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 2 // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1 // optional, default to 1.
-    }
-  };
+  desktop: {
+    breakpoint: { max: 4000, min: 1024 },
+    items: 3,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 function TestimoniesCard({ alumniAvater, alumniName, title, text }) {
   return (
     <div className="testimony-card">
@@ -34,41 +34,40 @@ function TestimoniesCard({ alumniAvater, alumniName, title, text }) {
   );
 }
 
-function Testimonies({deviceType}) {
-   
+function Testimonies({ deviceType }) {
   return (
-  
     <section className="testimonies-section">
       <h2 className="title">testimonies</h2>
-      <Carousel   swipeable={true}
-  draggable={true}
-  showDots={true}
-  responsive={responsive}
-  ssr={true} // means to render carousel on server-side.
-  infinite={true}
-  autoPlay={deviceType !== "mobile" ? true : false}
-  autoPlaySpeed={1000}
-  keyBoardControl={true}
-  customTransition="all .5"
-  transitionDuration={5000}
-  containerClass="carousel-container"
-  removeArrowOnDeviceType={["tablet", "mobile"]}
-  deviceType={deviceType}
-  dotListClass="custom-dot-list-style"
-  itemClass="carousel-item-padding-40-px">
-      {testimonies.map((testimony, i) => ( 
-      <div className="testimonies-container" key={testimony.alumniName}>
-        <TestimoniesCard
-          alumniAvater={testimony.alumniAvater}
-          alumniName={testimony.alumniName}
-          title={testimony.title}
-          text={testimony.text}
-        />
-      </div>
-      ))}
+      <Carousel
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        autoPlay={deviceType !== "mobile" ? true : false}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={5000}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        deviceType={deviceType}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
+        {testimonies.map((testimony, i) => (
+          <div className="testimonies-container" key={testimony.alumniName}>
+            <TestimoniesCard
+              alumniAvater={testimony.alumniAvater}
+              alumniName={testimony.alumniName}
+              title={testimony.title}
+              text={testimony.text}
+            />
+          </div>
+        ))}
       </Carousel>
     </section>
-
   );
 }
 
